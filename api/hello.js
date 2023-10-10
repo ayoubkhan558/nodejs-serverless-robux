0d6aeb32ad9833817a3752db6ac845d0bc85aafc
Muhammad Ayoub Khan <ayoubkhan558@hotmail.com>
2023-10-10 17:56:52 +0500

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -5,20 +5,23 @@ const axios = require('axios'); // Make sure you have axios installed
 module.exports = async (req, res) => {
   const { name = 'Ayoub' } = req.query;
   
-  try {
-    const cookie = '_|WARNING:-DO-NOT0BA'
+  // try {
+  //   const cookie = '_|WARNING:-DO-NOT0BA'
 
-    const currentUser = await noblox.setCookie(cookie)
-    const XCSRF = await noblox.getGeneralToken()
-    console.log(XCSRF)
+  //   const currentUser = await noblox.setCookie(cookie)
+  //   const XCSRF = await noblox.getGeneralToken()
+  //   console.log(XCSRF)
 
-    console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`);
+  //   console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`);
     
-    return res.json({
-      message: `Hi, ${name}!`,
-    });
-  } catch (error) {
-    console.error('An error occurred:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
+  //   return res.json({
+  //     message: `Hi, ${name}!`,
+  //   });
+  // } catch (error) {
+  //   console.error('An error occurred:', error);
+  //   return res.status(500).json({ error: 'Internal Server Error' });
+  // }
+  return res.json({
+    message: `Hi, ${name}!`,
+  });
 };
