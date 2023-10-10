535ed83fdf4d5427f6eb91d021abbdd0aca04f09
Muhammad Ayoub Khan <ayoubkhan558@hotmail.com>
2023-10-10 18:03:02 +0500

    CC

---
+++ api/hello copy.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// const noblox = require('noblox.js');
+// const axios = require('axios'); 
+
+module.exports = async (req, res) => {
+  const { name = 'Ayoub' } = req.query;
+  
+  // try {
+  //   const cookie = '_|WARNING:-DO-NOT0BA'
+
+  //   const currentUser = await noblox.setCookie(cookie)
+  //   const XCSRF = await noblox.getGeneralToken()
+  //   console.log(XCSRF)
+
+  //   console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`);
+    
+  //   return res.json({
+  //     message: `Hi, ${name}!`,
+  //   });
+  // } catch (error) {
+  //   console.error('An error occurred:', error);
+  //   return res.status(500).json({ error: 'Internal Server Error' });
+  // }
+  return res.json({
+    message: `Hi, ${name}!`,
+  });
+};
